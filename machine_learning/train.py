d6e46733b598be538a969e1ee8f8df9b61e85528
Gustav <gustavlagneborg@hotmail.com>
2021-09-14 13:42:49 +0200

    EBM classifier
    Creating a EBM classifier function

--- machine_learning/train.py
+++ machine_learning/train.py
@@ -1,5 +1,7 @@
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
+from interpret.glassbox import *
+from interpret import show
 
 
 def train_knn(X_train: pd.DataFrame, y_train: pd.DataFrame, n_neighbors=10):
@@ -14,3 +16,16 @@ def train_knn(X_train: pd.DataFrame, y_train: pd.DataFrame, n_neighbors=10):
     neigh.fit(X_train, y_train)
 
     return neigh
+
+def train_ebm(X_train: pd.DataFrame, y_train: pd.DataFrame):
+    """
+    Method for training a Explainable boosting machine on the provided data
+    :param X_train: Input variables of the training set
+    :param y_train: Target variable of the training set
+    :return: The trained EBM Classifier Model
+    """
+    ebm = ebm = ExplainableBoostingClassifier(random_state=1)
+    ebm.fit(X_train, y_train)
+    print("test")
+
+    return ebm
