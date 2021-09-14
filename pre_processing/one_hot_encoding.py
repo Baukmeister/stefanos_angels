732005ef5eb3e057e1780587f4f48d32d2c5aa29
Gustav <gustavlagneborg@hotmail.com>
2021-09-14 22:26:56 +0200

    Update one_hot_encoding.py

--- pre_processing/one_hot_encoding.py
+++ pre_processing/one_hot_encoding.py
@@ -2,6 +2,12 @@ import pandas as pd
 from pandas.core.frame import DataFrame
 
 def one_hot_encoding_function(dataset: pd.DataFrame, columns_to_encode=None):
+    """
+    Method for encoding categorical variables in a dataset
+    :param dataset: dataset with no encoding in it
+    :param columns_to_encode: columns in the dataset that will be replaced with encoding
+    :return: dataset with encoded variables 
+    """
     encoded_dataset = pd.get_dummies(dataset, columns = columns_to_encode)
     
     return encoded_dataset
