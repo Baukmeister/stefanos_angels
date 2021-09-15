ee725fb93d945e1d0993645541b174ffcbc311f7
Florian Bauernfeind <e11775814@student.tuwien.ac.at>
2021-09-15 13:56:00 +0200

    redo one-hot-encoder to be reusable in pipeline

--- pre_processing/normalize.py
+++ pre_processing/normalize.py
@@ -2,6 +2,11 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 
+def _cast_non_numeric_columns(dataset: pd.DataFrame):
+    object_cols = [col for col in dataset.columns if dataset[col].dtype == 'object']
+    dataset[object_cols] = dataset[object_cols].apply(pd.to_numeric)
+    return dataset
+
 def normalize(dataset: pd.DataFrame, method: str, excluded_cols=None):
     """
     Normalizes certain columns of a given dataset
@@ -10,20 +15,22 @@ def normalize(dataset: pd.DataFrame, method: str, excluded_cols=None):
     :param excluded_cols: Column names that should not be included in the normalization process (e.g. target variable)
     :return: The normalized dataset
     """
+    numeric_dataset = _cast_non_numeric_columns(dataset)
+
     if excluded_cols is None:
         excluded_cols = []
-    columns_to_scale = [col for col in dataset.columns if col not in excluded_cols]
-    scaled_dataset = dataset
+    columns_to_scale = [col for col in numeric_dataset.columns if col not in excluded_cols]
+    scaled_dataset = numeric_dataset
 
     if method == "identity":
         return scaled_dataset
 
     if method == "min-max":
-        scaled_dataset[columns_to_scale] = pd.DataFrame(MinMaxScaler().fit_transform(dataset[columns_to_scale]))
-        scaled_dataset.columns = dataset.columns
+        scaled_dataset[columns_to_scale] = pd.DataFrame(MinMaxScaler().fit_transform(numeric_dataset[columns_to_scale]))
+        scaled_dataset.columns = numeric_dataset.columns
         return scaled_dataset
 
     if method == "z":
-        scaled_dataset[columns_to_scale] = pd.DataFrame(StandardScaler().fit_transform(dataset[columns_to_scale]))
-        scaled_dataset.columns = dataset.columns
+        scaled_dataset[columns_to_scale] = pd.DataFrame(StandardScaler().fit_transform(numeric_dataset[columns_to_scale]))
+        scaled_dataset.columns = numeric_dataset.columns
         return scaled_dataset
--- pre_processing/one_hot_encoding.py
+++ pre_processing/one_hot_encoding.py
@@ -1,13 +1,26 @@
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 
 
-def one_hot_encoding_function(dataset: pd.DataFrame, columns_to_encode=None):
+def one_hot_encoding_function(dataset: pd.DataFrame, columns_to_encode=None, encoder: OneHotEncoder = None):
     """
     Method for encoding categorical variables in a dataset
     :param dataset: dataset with no encoding in it
     :param columns_to_encode: columns in the dataset that will be replaced with encoding
     :return: dataset with encoded variables 
     """
-    encoded_dataset = pd.get_dummies(dataset, columns=columns_to_encode)
+
+    columns_not_to_encode = [col for col in dataset.columns if col not in columns_to_encode]
+
+    if encoder is None:
+        encoder = OneHotEncoder(sparse=False).fit(dataset[columns_to_encode])
+
+    encoded_dataset = pd.concat(
+        [
+            dataset[columns_not_to_encode],
+            pd.DataFrame(encoder.transform(dataset[columns_to_encode]), columns=encoder.get_feature_names(columns_to_encode))
+        ],
+        axis=1
+    )
 
     return encoded_dataset
