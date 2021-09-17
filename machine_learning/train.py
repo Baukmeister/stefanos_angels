1e7adce1dff721a0a4926a82856126396c9f92d6
blaubarrsch <80891405+blaubarrsch@users.noreply.github.com>
2021-09-17 12:06:52 +0200

    Update train.py with SVM classifier
    I have now added the SVM as a classification model to the train.py file. Please let me know if this is the correct way of doing it or if I missed something

--- machine_learning/train.py
+++ machine_learning/train.py
@@ -14,3 +14,21 @@ def train_knn(X_train: pd.DataFrame, y_train: pd.DataFrame, n_neighbors=10):
 
     return neigh
 
+
+# SUPPORT VECTOR MACHINE
+
+from sklearn.svm import SVC
+
+def train_support_vector_machine(X_train: pd.DataFrame, y_train: pd.DataFrame, kernel='linear'):
+    """
+    Method for training an SVM Classifier on the provided data
+    :param X_train: Input variables of the training set
+    :param y_train: Target variable of the training set
+    :param kernel: Specifies the kernel type to be used in the algorithm
+    :return: The trained SVM Classifier Model
+    """
+    
+    svclassifier = SVC(kernel=kernel)
+    svclassifier.fit(X_train, Y_train)
+    
+    return svclassifier
