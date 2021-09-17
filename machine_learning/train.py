81f41d99a2b067d2deff998b7b4cde216f2b3415
nrguez <63447760+nrguez@users.noreply.github.com>
2021-09-17 10:44:03 +0200

    Update train.py
    - Add Logistic Regression Classifier

--- machine_learning/train.py
+++ machine_learning/train.py
@@ -14,3 +14,21 @@ def train_knn(X_train: pd.DataFrame, y_train: pd.DataFrame, n_neighbors=10):
 
     return neigh
 
+
+# LOGISTIC REGRESSION 
+
+from sklearn.linear_model import LogisticRegression
+
+def train_logistic_regression(X_train: pd.DataFrame, y_train: pd.DataFrame, solver='liblinear'):
+    """
+    Method for training a Logistic Regression Classifier on the provided data
+    :param X_train: Input variables of the training set
+    :param y_train: Target variable of the training set
+    :param solver: Algorithm to use in the optimization problem.
+    :return: The trained Logistic Regression Classifier Model
+    """
+    
+    logreg = LogisticRegression(solver=solver)
+    logreg.fit(X_train, y_train)
+
+    return logreg
