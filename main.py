dcfb127bfd5bba81a090e9c203d361ce3166348b
Gustav <gustavlagneborg@hotmail.com>
2021-10-18 07:35:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ CONFIG
                        'number of major vessels (0-3) colored by flourosopy',
                        'thal (3 = normal; 6 = fixed defect; 7 = reversable defect)']"""
 
-# categorical columns with drop2 as impute method
+# categorical columns with drop300 as impute method
 categorical_columns = ['sex (1 = male; 0 = female)',
                        'chest pain type (1 = typical angina; 2 = atypical angina; 3 = non-anginal pain; 4 = asymptomatic)',
                        'fasting blood sugar > 120 mg/dl (1 = true; 0 = false)',
